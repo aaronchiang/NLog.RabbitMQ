4640412a87321318490ae3902928e765b884e80f
Henrik <henrik@haf.se>
2011-08-12 00:42:27 +0200

    scaffolded build with logirel

---
+++ buildscripts/project_details.rb
@@ -0,0 +1,5 @@
+PROJECTS = {
+
+    :nr => {:title=>"NLog.Targets.RabbitMQ", :id=>"NLog.Targets.RabbitMQ", :dir=>"NLog.Targets.RabbitMQ", :test_dir=>"", :description=>"A RabbitMQ appender for NLog. See https://github.com/haf/NLog.RabbitMQ for documentation!", :authors=>"Henrik Feldt", :company=>"Logibit", :copyright=>"Copyright (c)Henrik Feldt 2011", :nuget_key=>"NLog.RabbitMQ", :ruby_key=>"nr", :guid=>"52f1a229-5604-4cf5-b797-d5b034e2e23e", :dependencies=>[], :create_package=>true},
+
+}
---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("NLog.Targets.RabbitMQ")]
+[assembly: AssemblyDescription("A RabbitMQ appender for NLog. See https://github.com/haf/NLog.RabbitMQ for documentation! edb3724e82cc5620e8888b6fa36a4f43e03f3601 - 2011-08-10 223543")]
+[assembly: AssemblyCompany("Logibit")]
+[assembly: AssemblyProduct("NLog.Targets.RabbitMQ")]
+[assembly: AssemblyCopyright("Copyright (c)Henrik Feldt 2011")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.1.22400")]
+[assembly: AssemblyFileVersion("0.0.1.22400")]
+
+[assembly: AssemblyInformationalVersion("0.0.1.22400")]
+[assembly: CLSCompliantAttribute(false)]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: Guid("52f1a229-5604-4cf5-b797-d5b034e2e23e")]
+
