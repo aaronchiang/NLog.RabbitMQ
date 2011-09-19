0509d56c0de44ba8eb508294118493afac06e214
Henrik <henrik@haf.se>
2011-09-19 19:03:36 +0200

    Released version v0.1.2

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,15 +1,15 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NLog.Targets.RabbitMQ")]
-[assembly: AssemblyDescription("A RabbitMQ appender for NLog. See https://github.com/haf/NLog.RabbitMQ for documentation! 96e42548748a076d24f83c41b62f84d30e19f9c8 - 2011-09-19 185422")]
+[assembly: AssemblyDescription("A RabbitMQ appender for NLog. See https://github.com/haf/NLog.RabbitMQ for documentation! e34bc8de693e1648cf9b43ea27729ffedbb5273b - 2011-09-19 190320")]
 [assembly: AssemblyCompany("Logibit")]
 [assembly: AssemblyProduct("NLog.Targets.RabbitMQ")]
 [assembly: AssemblyCopyright("Copyright (c)Henrik Feldt 2011")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.1.26219")]
-[assembly: AssemblyFileVersion("0.1.1.26219")]
+[assembly: AssemblyVersion("0.1.2.26219")]
+[assembly: AssemblyFileVersion("0.1.2.26219")]
 
-[assembly: AssemblyInformationalVersion("0.1.1.26219")]
+[assembly: AssemblyInformationalVersion("0.1.2.26219")]
 [assembly: CLSCompliantAttribute(false)]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: Guid("52f1a229-5604-4cf5-b797-d5b034e2e23e")]
